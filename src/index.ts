101a2da995931604ab324b4ae9026f2f84e6890d
Alex Corvi <alex@arrayy.com>
2017-03-26 19:14:43 +0300

    Refactored to the new api

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,35 @@
 import anchorme from "anchorme";
+import * as Fin from "finnlp";
 
+// detected links registry
 const links:string[] = [];
 
+// interceptor
+Fin.interceptors.push(function(string:string){
+	anchorme(string,{list:true})
+	.forEach((match:any,index:number)=>{
+		console.log(match.raw);
+		links[index] = match.raw;
+		string = string.split(match.raw).join("DetectedLINK"+index);
+	});
+	return string;
+});
 
-const interceptor = {
-	type:"interceptor",
-	function (string:string):string {
-		(anchorme(string,{list:true}) as string[])
-		.forEach((match,index)=>{
-			links[index] = match;
-			string.split(match).join("DetectedLINK"+index);
-		});
-		return string;
+
+// detector
+declare module "finnlp" {
+	export interface Run {
+		links:()=>string[][];
 	}
-};
+}
 
-const detector = {
-	type:"detector",
-	extension:(input:any) => {
-		input.links = function(){
-			return links;
-		};
-		return input;
-	}
+Fin.Run.prototype.links = function(this:Fin.Run){
+	const result:string[][] = [];
+	this.sentences.forEach((sentence,sentenceIndex)=>{
+		result[sentenceIndex] = sentence.tokens
+		.filter(x=>/^(DetectedLINK)(\d)+$/.test(x))
+		.map(link=>parseInt(link.replace(/^DetectedLINK/,"")))
+		.map(indexInTheRegistry=>links[indexInTheRegistry]);
+	});
+	return result;
 };
-
-export default [interceptor,detector];
